d9fcc8ae326744492e791952d1917216df3e1827
Hilary Madeley Calva Camacho <105516501+Hilary-Madelein@users.noreply.github.com>
2023-07-02 21:04:05 -0500

    Add files via upload

---
+++ khat-tf2/models/cuenta.js
@@ -0,0 +1,18 @@
+'use strict';
+const { UUIDV4 } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    const cuenta = sequelize.define('cuenta', {
+        external_id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4},
+        estado: { type: DataTypes.BOOLEAN, defaultValue: true },
+        correo: { type: DataTypes.STRING(50), allowNull: false },
+        clave: { type: DataTypes.STRING(150), allowNull: false }
+    }, {
+        freezeTableName: true
+    });
+
+    cuenta.associate = function (models){
+        cuenta.belongsTo(models.persona, {foreignKey: 'id_persona'});
+    }
+
+    return cuenta;
+};
---
+++ khat-tf2/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/privado/:external', function(req, res, next) {
+  require('dotenv').config();
+  const llave=req.params.external;
+  const env=process.env.KEY_SQ;
+  if(llave === env){
+    var models=require('./../models');
+    models.sequelize.sync().then(()=>{
+      console.log('Se ha conectado la bd');
+      res.send('OK!');
+    }).catch(err=>{
+      console.log(err,"Hubo un error");
+      res.send("Error");
+    })
+  }else{
+    res.send("Llave incorrecta!");
+  }
+});
+
+module.exports = router;
